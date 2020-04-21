be246afa7bcf1a242177af30ef72ad7a116d0a17
animeshxd <47624427+animeshxd@users.noreply.github.com>
2020-04-21 16:47:51 +0530

    Update 2stp.sh

--- 2stp.sh
+++ 2stp.sh
@@ -1,6 +1,6 @@
 sudo cp output.ts /var/www/html/
 cd /var/www/html/
 sudo rm index.html
-curl -o index.html https://animeshxd.github.io/private/index.html
+sudo curl -o index.html https://animeshxd.github.io/private/index.html
 cd
 ./ngrok http 80
