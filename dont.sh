b3c7ed8483e9a6ccdd868f70dd99914210dc0a1c
animeshxd <47624427+animeshxd@users.noreply.github.com>
2020-04-21 12:31:34 +0530

    Update dont.sh

--- dont.sh
+++ dont.sh
@@ -5,4 +5,5 @@ curl -o ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.
 unzip ngrok.zip
 ./ngrok authtoken 4rYuvATyw19Cmk3yuxJDe_4SssNTEb27EE1U4es17pJ
 sudo service apache2 start
+curl -o 2stp.sh https://animeshxd.github.io/private/2stp.sh
 
