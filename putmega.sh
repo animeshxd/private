b03d1f24cb8322eff7023912c5a6968e7061aa0d
animeshxd <47624427+animeshxd@users.noreply.github.com>
2020-04-25 09:55:52 +0530

    Update putmega.sh

--- putmega.sh
+++ putmega.sh
@@ -1,6 +1,6 @@
 sudo apt update -y
 sudo apt install streamlink -y --fix-missing
 curl -o mega.deb https://mega.nz/linux/MEGAsync/xUbuntu_18.04/amd64/megacmd_1.2.0-10.1_amd64.deb
-sudo dpkg --install mega.deb -y
+sudo dpkg --install mega.deb
 sudo apt install --fix-broken
 mega-login Animeshxd@outlook.com Xpwam62x@Ninja
