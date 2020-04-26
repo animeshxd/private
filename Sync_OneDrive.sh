dc471bca9e76c040207c58b99ad8c7d886bfcc59
animeshxd <47624427+animeshxd@users.noreply.github.com>
2020-04-26 08:57:34 +0530

    Create Sync_OneDrive.sh

---
+++ Sync_OneDrive.sh
@@ -0,0 +1,13 @@
+sudo apt update -y
+sudo apt install streamlink -y --fix-missing
+sudo apt install libcurl4-openssl-dev -y --fix-missing
+sudo apt install libsqlite3-dev -y --fix-missing
+sudo snap install --classic dmd && sudo snap install --classic dub
+git clone https://github.com/skilion/onedrive.git
+cd onedrive
+make
+sudo make install
+make DC=ldmd2
+mkdir -p ~/.config/onedrive
+cp ./config ~/.config/onedrive/config
+nano ~/.config/onedrive/config
