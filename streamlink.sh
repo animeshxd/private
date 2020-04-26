cdbfe05b84f3d2f8fc61c3886fe6031e03d12f46
animeshxd <47624427+animeshxd@users.noreply.github.com>
2020-04-26 09:23:40 +0530

    Update streamlink.sh

--- streamlink.sh
+++ streamlink.sh
@@ -2,7 +2,7 @@ sudo apt upgrade -y
 sudo apt update -y
 sudo apt install streamlink unzip apache2 -y --fix-missing
 curl -o ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip  .zip
+unzip ngrok.zip
 ./ngrok authtoken 4rYuvATyw19Cmk3yuxJDe_4SssNTEb27EE1U4es17pJ
 sudo service apache2 start
 curl -o 2stp.sh https://animeshxd.github.io/private/two.sh
