a9fa8e3a2a09c73c7cd4bafea42f2fb2df6c6111
Animesh Murmu <47624427+animeshxd@users.noreply.github.com>
2020-06-20 19:14:05 +0530

    Add files via upload

--- vindx.sh
+++ vindx.sh
@@ -1,18 +1,19 @@
-RED='\033[0;32m'
+GREEN='\033[0;32m'
 NC='\033[0;97m'
-rm stmli.sh refcurl.sh yt-dl.sh
-curl -o stmli.sh https://animeshxd.github.io/private/stmli.sh
-curl -o refcurl.sh https://animeshxd.github.io/private/refcurl.sh
-curl -o yt-dl.sh https://animeshxd.github.io/private/yt-dl.sh
-echo -e " ${GREEN}Select Downloader (curl/streamlink/yt-dl)
+echo -e "${GREEN} welcome"
+rm stmli refcurl yt-dl
+curl -o stmli https://animeshxd.github.io/private/stmli.sh
+curl -o refcurl https://animeshxd.github.io/private/refcurl.sh
+curl -o yt-dl https://animeshxd.github.io/private/yt-dl.sh
+echo  "Select Downloader (curl/streamlink/yt-dl)
 1. streamlink
 2. curl
 3. youtube-dl"
-read choice
-echo -e "${NC}"
+read downloader
 case "$downloader" in 
-  1 ) bash stmli.sh;;
-  2 ) bash refcurl.sh;;
-  3 ) bash yt-dl.sh;;
+  1 ) bash stmli;;
+  2 ) bash refcurl;;
+  3 ) bash yt-dl;;
   * ) echo "invalid";;
 esac
+chmod +x stmli refcurl yt-dl
