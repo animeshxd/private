9e2c23aed63696c9934bee6ad33bcf6d802e95da
Animesh Murmu <47624427+animeshxd@users.noreply.github.com>
2020-06-20 17:27:14 +0530

    Add files via upload

---
+++ vindx.sh
@@ -0,0 +1,24 @@
+RED='\033[0;32m'
+NC='\033[0;97m'
+Enter Folder Name
+read Folder
+mkdir $Folder
+
+rm stml.sh refcurl.sh yt-dl.sh
+curl -o $Folder/stmli.sh https://animeshxd.github.io/private/stmli.sh
+curl -o $Folder/refcurl.sh https://animeshxd.github.io/private/refcurl.sh
+curl -o $Folder/yt-dl.sh https://animeshxd.github.io/private/yt-dl.sh
+
+
+echo -e " ${GREEN}Select Downloader (curl/streamlink/yt-dl)
+1. streamlink
+2. curl
+3. youtube-dl"
+read choice
+echo -e "${NC}"
+case "$downloader" in 
+  1 ) bash stmli.sh;;
+  2 ) bash refcurl.sh;;
+  3 ) bash yt-dl.sh;;
+  * ) echo "invalid";;
+esac
