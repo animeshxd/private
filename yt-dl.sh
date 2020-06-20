77902fdc0a35ed44f077618a20f35fef8812e15e
Animesh Murmu <47624427+animeshxd@users.noreply.github.com>
2020-06-20 17:53:30 +0530

    Update yt-dl.sh

--- yt-dl.sh
+++ yt-dl.sh
@@ -3,4 +3,4 @@ NC='\033[0;97m'
 echo "Enter the media URL of the file you want to download"
 read link
 
-youtube-dl -f mp4  --all-subs --yes-playlist --geo-bypass $link
+youtube-dl -f mp4 -o $Folder/output.mp4 --all-subs --yes-playlist --geo-bypass $link
