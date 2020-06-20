63ccdad28cc12ea5bdc113fedb7621996849c1d3
Animesh Murmu <47624427+animeshxd@users.noreply.github.com>
2020-06-20 22:30:22 +0530

    Update vindx.sh

--- vindx.sh
+++ vindx.sh
@@ -1,19 +1,19 @@
 GREEN='\033[0;32m'
 NC='\033[0;97m'
+
 echo -e "${GREEN} welcome"
-rm stmli refcurl yt-dl
-curl -o stmli https://animeshxd.github.io/private/stmli.sh
-curl -o refcurl https://animeshxd.github.io/private/refcurl.sh
-curl -o yt-dl https://animeshxd.github.io/private/yt-dl.sh
+curl -o hls-ts https://animeshxd.github.io/private/hls-ts
+curl -o dl-mp4 https://animeshxd.github.io/private/dl-mp4
+curl -o yt-dl https://animeshxd.github.io/private/yt-dl
 echo  "Select Downloader (curl/streamlink/yt-dl)
 1. streamlink
 2. curl
 3. youtube-dl"
 read downloader
 case "$downloader" in 
-  1 ) bash stmli;;
-  2 ) bash refcurl;;
+  1 ) bash hls-ts;;
+  2 ) bash dl-mp4;;
   3 ) bash yt-dl;;
   * ) echo "invalid";;
 esac
-chmod +x stmli refcurl yt-dl
+chmod +x hls-ts dl-mp4 yt-dl
